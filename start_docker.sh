97947d7e9144973166e056c151350308e2a0d9a8
Johannes Kühnel <j.kuehnel@number13.de>
2017-01-16 17:18:03 +0100

    foo

--- start_docker.sh
+++ start_docker.sh
@@ -15,3 +15,19 @@ docker run -i -d \
 	-e SPAM_PASS=spamassasinpw \
 	-e MAIL_SERVER_DOMAIN=mail.tutomail.de mailserver
 
+
+docker run -t -i \
+	-p 25:25 \
+	-p 587:587 \
+	-p 110:110 \
+	-p 143:143 \
+	-p 995:995 \
+	-p 993:993 \
+	-v opendkim:/etc/opendkim/ \
+	-v vmail:/var/vmail/ \
+	-v log:/var/log/ \
+	-v postfix:/etc/postfix/ptroverride/ \
+	-e SQL_PASSWORD=meinsqlpassword \
+	-e SPAM_PASS=spamassasinpw \
+	-e MAIL_SERVER_DOMAIN=mail.tutomail.de mailserver
+
